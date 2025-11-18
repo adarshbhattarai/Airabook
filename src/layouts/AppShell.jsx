42dad125273d7fb8706ac0234156a76dda923d9d
Adarsh <adarsh.bhat7@gmail.com>
2025-11-17 23:21:10 -0700

    Update

--- src/layouts/AppShell.jsx
+++ src/layouts/AppShell.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Sidebar from '@/components/navigation/Sidebar';
 import MobileTopBar from '@/components/navigation/MobileTopBar';
+import AppHeader from '@/components/navigation/AppHeader';
 
 const AppShell = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -11,6 +12,7 @@ const AppShell = ({ children }) => {
 
       <div className="flex-1 flex flex-col overflow-hidden">
         <MobileTopBar onMenuClick={() => setSidebarOpen(true)} />
+        <AppHeader />
         <main className="flex-1 overflow-y-auto">
           {children}
         </main>
